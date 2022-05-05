8df5c066b1f632b5b684df269b3dbb3d40864058
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 12:35:52 +0430

    feat - modify schema.sql and data.sql
    add queries for authorities table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO users (email, password)
-VALUES ('foo@gmail.com', '123'),
-       ('bar@email.com', '123');
+INSERT INTO users (email, password, enabled)
+VALUES ('foo@gmail.com', '123', true),
+       ('bar@email.com', '123', true);
+
+INSERT INTO authorities (email, authority)
+VALUES ('foo@gmail.com', 'ADMIN'),
+       ('bar@email.com', 'USER');
