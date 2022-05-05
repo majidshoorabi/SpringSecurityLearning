3f88242650f4f08dda0a8ba44d8df4cdd63ae0ff
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 15:35:59 +0430

    feat - implements UserService from UserServiceDetails

--- src/main/java/com/github/majidshoorabi/security/user/UserService.java
+++ src/main/java/com/github/majidshoorabi/security/user/UserService.java
@@ -1,6 +1,9 @@
 package com.github.majidshoorabi.security.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,15 +13,14 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
-public class UserService {
-
-    private final UserRepository userRepository;
+public class UserService implements UserDetailsService {
 
     @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
     }
-
-
 }
 
