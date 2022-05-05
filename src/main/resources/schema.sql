eb2d3a4de99ca4607beab62917ac585e413649d8
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 12:35:52 +0430

    feat - modify schema.sql and data.sql
    add queries for authorities table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,19 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE USERS
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
-    email      VARCHAR(250) DEFAULT NULL,
-    password VARCHAR(250) DEFAULT NULL
+    email      VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    enabled BOOLEAN NOT NULL
+
 );
+
+
+DROP TABLE IF EXISTS authorities;
+
+CREATE TABLE authorities(
+    email VARCHAR (250) NOT NULL,
+    authority VARCHAR (250) NOT NULL,
+    constraint fk_authorities_users FOREIGN key(email) REFERENCES users(email)
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON authorities (email, authority);
