c49b0633cec57670a8be091773369432118d3f2c
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 16:43:45 +0430

    feat - add @PreAuthorize("hasAuthority('ADMIN')") for endpoints

--- src/main/java/com/github/majidshoorabi/security/user/UserController.java
+++ src/main/java/com/github/majidshoorabi/security/user/UserController.java
@@ -2,6 +2,7 @@ package com.github.majidshoorabi.security.user;
 
 import com.github.majidshoorabi.security.user.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +31,7 @@ public class UserController {
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String adminPage(Model model) {
         model.addAttribute("users", userService.findAll());
         return "admin";
