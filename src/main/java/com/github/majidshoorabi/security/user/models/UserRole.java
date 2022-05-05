d4c2860f75fea913ce556b37e0b1e7198b16a73a
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 15:35:23 +0430

    feat - implements UserRole from GrantedAuthority

--- src/main/java/com/github/majidshoorabi/security/user/models/UserRole.java
+++ src/main/java/com/github/majidshoorabi/security/user/models/UserRole.java
@@ -1,13 +1,20 @@
 package com.github.majidshoorabi.security.user.models;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * @author majid.shoorabi
  * @created 2022-05-May
  * @project peysaz
  */
 
-public enum UserRole {
+public enum UserRole implements GrantedAuthority {
 
     ADMIN,
-    USER
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return this.name();
+    }
 }
