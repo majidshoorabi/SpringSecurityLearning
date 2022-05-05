1a846b311a62013c9e9fd0805002d526439aab3f
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 16:11:14 +0430

    feat - add findAll, findById, deleteById, registerUser methods in UserService

--- src/main/java/com/github/majidshoorabi/security/user/UserService.java
+++ src/main/java/com/github/majidshoorabi/security/user/UserService.java
@@ -1,10 +1,14 @@
 package com.github.majidshoorabi.security.user;
 
+import com.github.majidshoorabi.security.user.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author majid.shoorabi
@@ -22,5 +26,22 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email);
     }
-}
 
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public void deleteById(User user) {
+        userRepository.deleteById(user.getId());
+    }
+
+
+    @Transactional
+    public void registerUser(User user) {
+        userRepository.saveAndFlush(user);
+    }
+}
