f997efca71723d268f89d307f144360602fd6eaa
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 12:56:48 +0430

    feat - add UserRole enum

---
+++ src/main/java/com/github/majidshoorabi/security/user/models/UserRole.java
@@ -0,0 +1,13 @@
+package com.github.majidshoorabi.security.user.models;
+
+/**
+ * @author majid.shoorabi
+ * @created 2022-05-May
+ * @project peysaz
+ */
+
+public enum UserRole {
+
+    ADMIN,
+    USER
+}
