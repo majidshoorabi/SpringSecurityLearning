cb7983208035ded6c3acb5cd559aff5dd3798c23
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 12:57:48 +0430

    feat - add SecurityConfig.java
    Jdbc Authentication config

---
+++ src/main/java/com/github/majidshoorabi/security/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.github.majidshoorabi.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+/**
+ * @author majid.shoorabi
+ * @created 2022-05-May
+ * @project peysaz
+ */
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/login").permitAll()     // pages that don't need to authentication - you must add your login page here
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login")    // introduce your login page
+                .usernameParameter("email");        // change login username parameter
+    }
+
+
+    /**
+     * JDBC Authentication
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(this.dataSource)
+                .usersByUsernameQuery("SELECT email, password, enabled FROM users WHERE email=?")       // Because we change some default column names and table names we should write custom queries
+                .authoritiesByUsernameQuery("SELECT email, authority FROM authorities WHERE email=?");
+    }
+
+
+    /**
+     *  For custom in memoryAuthentication you need a passwordEncoder
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();       // Password is plain text
+    }
+}
