b74e9c66d6718003659647f413e139da5dcbb500
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 15:32:44 +0430

    feat - implements User from UserDetails

--- src/main/java/com/github/majidshoorabi/security/user/models/User.java
+++ src/main/java/com/github/majidshoorabi/security/user/models/User.java
@@ -1,12 +1,11 @@
 package com.github.majidshoorabi.security.user.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -15,13 +14,10 @@ import java.util.List;
  * @project peysaz
  */
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,10 +27,88 @@ public class User implements Serializable {
     private String password;
     private Boolean enabled = Boolean.TRUE;
 
-    @ElementCollection(targetClass = UserRole.class)
+    @ElementCollection(targetClass = UserRole.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "authorities",
             joinColumns = @JoinColumn(name = "email", referencedColumnName = "email"))
     @Enumerated(EnumType.STRING)
-    private List<UserRole> authorities;
+    private List<UserRole> authority;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authority;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<UserRole> getUserRoles() {
+        return authority;
+    }
+
+    public void setUserRoles(List<UserRole> userRoles) {
+        this.authority = userRoles;
+    }
 
 }
