06bbca1a15086f5c62f50d09bb717ab09198dae0
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-05 16:12:22 +0430

    feat - add adminPage, getUser, deleteById, registerPage, delete,register  methods in UserController

--- src/main/java/com/github/majidshoorabi/security/user/UserController.java
+++ src/main/java/com/github/majidshoorabi/security/user/UserController.java
@@ -1,8 +1,10 @@
 package com.github.majidshoorabi.security.user;
 
+import com.github.majidshoorabi.security.user.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author majid.shoorabi
@@ -10,15 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @project peysaz
  */
 
+
 @Controller
 public class UserController {
 
-    private final UserService userService;
-
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    private UserService userService;
 
     @GetMapping("")
     public String indexPage() {
@@ -31,12 +30,46 @@ public class UserController {
     }
 
     @GetMapping("/admin")
-    public String adminPage() {
+    public String adminPage(Model model) {
+        model.addAttribute("users", userService.findAll());
         return "admin";
+    }
+
+
+    @GetMapping("/user/get/{id}")
+    public @ResponseBody User getUser(@PathVariable("id") Long id) {
+        return userService.findById(id);
+    }
+
+
+    @GetMapping(value = "/admin/register")
+    public String registerPage(Model model) {
+        model.addAttribute("user", new User());
+        return "registerUser";
+    }
+
+    @GetMapping(value = "/admin/edit/{id}")
+    public String registerPage(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.findById(id));
+        return "registerUser";
+    }
+
+    @GetMapping(value = "/admin/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteById(userService.findById(id));
+        return "redirect:/admin";
+    }
+
+    @PostMapping(value = "/admin/register")
+    public String register(@ModelAttribute(name = "user") User user) {
+        userService.registerUser(user);
+        return "redirect:/admin";
     }
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
+
+
 }
