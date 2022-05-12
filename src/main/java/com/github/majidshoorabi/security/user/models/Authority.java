dd6e67881ebcebee0f87f533af7b2acdddcc484a
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-12 16:49:23 +0430

    feat - Add Authority.java enum

--- src/main/java/com/github/majidshoorabi/security/user/models/Authority.java
+++ src/main/java/com/github/majidshoorabi/security/user/models/Authority.java
@@ -4,14 +4,17 @@ import org.springframework.security.core.GrantedAuthority;
 
 /**
  * @author majid.shoorabi
- * @created 2022-05-May
+ * @created 2022-12-May
  * @project peysaz
  */
 
-public enum UserRole implements GrantedAuthority {
+public enum Authority implements GrantedAuthority {
 
-    ADMIN,
-    USER;
+    OP_ACCESS_ADMIN,
+    OP_NEW_USER,
+    OP_EDIT_USER,
+    OP_DELETE_USER,
+    OP_ACCESS_USER;
 
     @Override
     public String getAuthority() {
