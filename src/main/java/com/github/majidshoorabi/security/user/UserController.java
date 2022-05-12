181202eef036cfbae1868871c8590baa243eb1de
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-12 16:51:09 +0430

    chore - set operation permission with PreAuthorize annotation for endpoints

--- src/main/java/com/github/majidshoorabi/security/user/UserController.java
+++ src/main/java/com/github/majidshoorabi/security/user/UserController.java
@@ -27,33 +27,36 @@ public class UserController {
     }
 
     @GetMapping("/user")
+    @PreAuthorize("hasAuthority('OP_ACCESS_USER')")
     public String userPage() {
         return "user";
     }
 
     @GetMapping("/admin")
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('OP_ACCESS_ADMIN')")
     public String adminPage(Model model) {
         model.addAttribute("users", userService.findAll());
         return "admin";
     }
 
-
     @GetMapping("/user/get/{id}")
     @PostAuthorize("returnObject.email == authentication.name")
-    public @ResponseBody User getUser(@PathVariable("id") Long id) {
+    public @ResponseBody
+    User getUser(@PathVariable("id") Long id) {
         return userService.findById(id);
     }
 
 
     @GetMapping(value = "/admin/register")
-    public String registerPage(Model model) {
+    @PreAuthorize("hasAuthority('OP_NEW_USER')")
+    public String edit(Model model) {
         model.addAttribute("user", new User());
         return "registerUser";
     }
 
     @GetMapping(value = "/admin/edit/{id}")
-    public String registerPage(Model model, @PathVariable("id") Long id) {
+    @PreAuthorize("hasAuthority('OP_EDIT_USER')")
+    public String edit(Model model, @PathVariable("id") Long id) {
         model.addAttribute("user", userService.findById(id));
         return "registerUser";
     }
