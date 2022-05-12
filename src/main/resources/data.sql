7d3253728acaed1e2f4c7f1b87d0dbb36d20d33e
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-12 17:32:47 +0430

    chore - edit data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,13 +4,16 @@ VALUES ('foo@gmail.com', '123', true),
 
 INSERT INTO roles (name)
 VALUES ('ADMIN'),
-       ('ADMIN 2');
+       ('ADMIN 2'),
+       ('USER');
 
 INSERT INTO users_roles(user_id, role_id)
 VALUES (1, 1),
-       (1, 2);
+       (1, 2),
+       (2, 3);
 
 
 INSERT INTO role_authorities(role_id, authorities)
 VALUES (1, 0),
-       (1, 1);
+       (1, 1),
+       (3, 4);
