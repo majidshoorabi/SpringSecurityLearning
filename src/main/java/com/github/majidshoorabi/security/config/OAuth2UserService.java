50f720b7f54e600ed0e590f76215305f2342c18c
majijdshoorabi <majidshoorabi@gmail.com>
2022-05-19 12:38:30 +0430

    feat - add users that login through OAuth2 to database

--- src/main/java/com/github/majidshoorabi/security/config/OAuth2UserService.java
+++ src/main/java/com/github/majidshoorabi/security/config/OAuth2UserService.java
@@ -1,6 +1,8 @@
 package com.github.majidshoorabi.security.config;
 
+import com.github.majidshoorabi.security.user.UserRepository;
 import com.github.majidshoorabi.security.user.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -16,19 +18,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class OAuth2UserService extends DefaultOAuth2UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User =super.loadUser(userRequest);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
 
 //        for (String key : oAuth2User.getAttributes().keySet()) {
 //            System.out.println(key + " : "+oAuth2User.getAttribute(key));
 //        }
 
-        User user = new User();
-        user.setEmail(oAuth2User.getAttribute("email"));
+        User user = userRepository.findByEmail(oAuth2User.getAttribute("email"));
+        if (user == null) {
+            user = new User();
+            user.setEmail(oAuth2User.getAttribute("email"));
+        }
         user.setName(oAuth2User.getAttribute("name"));
         user.setPicture(oAuth2User.getAttribute("picture"));
-
+        user = userRepository.save(user);
         return user;
     }
 }
